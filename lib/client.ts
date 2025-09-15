d823682bc9d2606ba9b0102178e8bc87644d6726
Hui Liu <liuyenhui@gmail.com>
2025-09-15 23:24:43 +0800

    update add app_key...

--- lib/client.ts
+++ lib/client.ts
@@ -1,11 +1,22 @@
 class Client {
     // name property
-    name: string;
-    constructor(name: string) {
-        this.name = name;
+    app_key: string;
+    app_secret: string;
+    sid: string;
+    base_url: string;
+    constructor(
+        app_key: string,
+        app_secret: string,
+        sid: string,
+        base_url: string = "https://api.wangdian.cn/openapi2/"
+    ) {
+        this.app_key = app_key;
+        this.app_secret = app_secret;
+        this.sid = sid;
+        this.base_url = base_url;
     }
-    sayHello() {
-        console.log(`Hello, ${this.name}!`);
+    info() {
+        console.log(`Client app_key: ${this.app_key}, app_secret: ${this.app_secret}, sid: ${this.sid}, base_url: ${this.base_url}`);
     }
 }
 export {
--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { Client } from "@liuhui/wangdian";
 
 async function main() {
-    const client = new Client("John");
-    client.sayHello();
+    const client = new Client("cmdz02-yya", "7a3dc75ae7fee8368b89f2c82ac40d12", "cmdz02");
+    client.info();
 }
 
 await main();
